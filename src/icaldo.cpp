3c0131144749a3752350407c904a590df4b49fcf
Elvis Dukaj <elvis.dukaj@gmail.com>
2015-09-12 12:19:17 +0200

    Aggiunto unita' di misura per il DAC

--- src/icaldo.cpp
+++ src/icaldo.cpp
@@ -4,37 +4,43 @@
 
 using namespace std;
 
+inline void WaitUser( bool showMessage = false)
+{
+    static string line;
+    // wait for 'Enter'
+    cout << "Press 'Enter' to continue" << endl;
+    getline( cin, line );
+}
+
 int main()
 {
     string device{"/dev/i2c-1"};
 
     try
     {
-        int address = 0x62;
-        cout << "opening at address " << hex << address << dec << endl;
-        dac::mcp4725 d1( device, address);
-        cout << "done..." << endl;
-        for( double v : { 0.1, 0.5, 1.0, 3.0, 4.0, 4.5, 5.0 } )
+        for( auto address : { 0x62, 0x63 } )
         {
-            cout << "setting v to " << v << "...";
-            d1.set( v );
-            string line;
-            getline( cin, line );
-            cout << "done..." << endl;
-        }
+            cout << "Opening dac at address " << hex << address << dec << endl;
+            dac::mcp4725 dev( device, address );
 
+            cout << "STARTIN VOLTAGE TEST" << endl;
+            for( auto v : { 0.1_V, 0.5_V, 1.0_V, 3.0_V, 4.0_V, 4.5_V, 5.0_V } )
+            {
+                dev.set( v );
+                cout << "setting voltage to " << v << "V" << endl;
+                WaitUser();
+            }
+            cout << "TEST FINISHED" << endl;
 
-        address = 0x63;
-        cout << "opening at address " << hex << address << dec << endl;
-        dac::mcp4725 d2( device, address);
-        cout << "done..." << endl;
-        for( double v : { 0.1, 0.5, 1.0, 3.0, 4.0, 4.5, 5.0 } )
-        {
-            cout << "setting v to " << v << "...";
-            d2.set( v );
-            string line;
-            getline( cin, line );
-            cout << "done..." << endl;
+            cout << "STARTIN PERCENTAGE TEST" << endl;
+            for( auto v : { 0.0_percent, 0.1_percent, 0.5_percent, 100.0_percent } )
+            {
+                dev.set( v );
+                cout << "setting voltage to " << v * 100.0 << "%" << endl;
+                WaitUser();
+            }
+
+            cout << endl;
         }
     }
     catch( const std::exception& exc )
--- src/mcp4725.cpp
+++ src/mcp4725.cpp
@@ -12,16 +12,15 @@ enum class commands : uint8_t
 
 mcp4725::mcp4725(
         std::string devname,
-        i2c::address_t address,
-        double vdd
+        i2c::address_t address
         )
-    : mDevice{ i2c::create( devname, address ) },
-      mVDD{ vdd }
+    : mDevice{ i2c::create( devname, address ) }
 {
+    mVDD = 5.0;
 }
 
 
-void mcp4725::set(double val, bool writeToEEPROM)
+void mcp4725::set(Volt val, bool writeToEEPROM)
 {
     // convert val into
     auto d = voltageToRegister( val );
@@ -37,11 +36,28 @@ void mcp4725::set(double val, bool writeToEEPROM)
     );
 }
 
+
+void mcp4725::set( Percent val, bool writeToEEPROM )
+{
+    // convert val into
+    auto d = percentToRegister( val );
+
+    mDevice->write(
+    {
+        writeToEEPROM ?
+        static_cast< uint8_t >(commands::write_dac_and_eeprom) :
+        static_cast< uint8_t >(commands::write_dac),
+        static_cast< uint8_t >((d >> 4) & 0xFF),
+        static_cast< uint8_t >((d << 4) & 0xFF)
+    }
+    );
+}
+
 double mcp4725::get() const
 {
 }
 
-int mcp4725::voltageToRegister( double Vout )
+int mcp4725::voltageToRegister( long double Vout )
 {
 
     //       4096
@@ -50,8 +66,16 @@ int mcp4725::voltageToRegister( double Vout )
 
     // D = Vout * K
 
-    static const auto K = 4096.0 / mVDD;
+    static const auto K = 4095.0 / mVDD;
     return static_cast< int >( round( Vout * K ) );
+}
+
+
+int mcp4725::percentToRegister( long double perc )
+{
+    // percent : 100 = x : 4096
+    // x = percent * 4096 / 100
+    return round( perc * 4095.0L );
 }
 
 } // dac
